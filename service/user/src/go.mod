629c6fe89a899071b96b739efa4c3120033049b8
bryan.wu <bryan.wu@ringcentral.com>
2022-10-26 10:57:27 +0800

    use gqlgen generate code

--- service/user/src/go.mod
+++ service/user/src/go.mod
@@ -4,33 +4,25 @@ go 1.19
 
 require (
 	github.com/99designs/gqlgen v0.17.20
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/vektah/gqlparser/v2 v2.5.1
 )
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/machinebox/graphql v0.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 )
 
 require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/go-chi/chi v1.5.4
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
-	github.com/mitchellh/mapstructure v1.3.1 // indirect
+	github.com/mitchellh/mapstructure v1.3.1
 	github.com/nicelogic/auth v0.1.0
 	github.com/nicelogic/cassandra v0.1.0
 	github.com/nicelogic/config v0.1.0
 	github.com/nicelogic/errs v0.1.0
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.8.1 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/exp v0.0.0-20221025133541-111beb427cde
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.1.12 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- service/user/src/graph/schema.resolvers.go
+++ service/user/src/graph/schema.resolvers.go
@@ -15,43 +15,36 @@ import (
 	"golang.org/x/exp/maps"
 )
 
-// Mutation returns generated.MutationResolver implementation.
-func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
-
-// Query returns generated.QueryResolver implementation.
-func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
-
-type mutationResolver struct{ *Resolver }
-type queryResolver struct{ *Resolver }
-
-func (r *mutationResolver) UpdateUser(ctx context.Context, changes map[string]interface{}) (responseUser *model.User, err error) {
+// UpdateUser is the resolver for the updateUser field.
+func (r *mutationResolver) UpdateUser(ctx context.Context, changes map[string]interface{}) (*model.User, error) {
 	user, err := auth.GetUser(ctx)
 	if err != nil {
-		return
+		return nil, err
 	}
 	fmt.Printf("user: %v update: %v\n", user.Id, changes)
 
 	gql, variables, err := cassandra.UpdateUserGql(changes)
 	if err != nil {
-		return
+		return nil, err
 	}
 	variables["id"] = user.Id
 	response, err := r.CassandraClient.Mutation(gql, variables)
 	if err != nil {
-		return
+		return nil, err
 	}
 	fmt.Printf("response: %v\n", response)
-	responseUser, err = cassandra.GetUpdatedUserFromResponse(response)
+	responseUser, err := cassandra.GetUpdatedUserFromResponse(response)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return
+	return responseUser, nil
 }
 
-func (r *queryResolver) Me(ctx context.Context) (me *model.User, err error) {
+// Me is the resolver for the me field.
+func (r *queryResolver) Me(ctx context.Context) (*model.User, error) {
 	user, err := auth.GetUser(ctx)
 	if err != nil {
-		return
+		return nil, err
 	}
 	fmt.Printf("user: %v query own info\n", user.Id)
 
@@ -60,23 +53,24 @@ func (r *queryResolver) Me(ctx context.Context) (me *model.User, err error) {
 	}
 	response, err := r.CassandraClient.Query(cassandra.QueryUserByIdGql, variables)
 	if err != nil {
-		return
+		return nil, err
 	}
 	fmt.Printf("response: %v\n", response)
-	me, err = cassandra.GetUserFromQueryUserByIdResponse(response)
+	me, err := cassandra.GetUserFromQueryUserByIdResponse(response)
 	if err != nil {
-		return
+		return nil, err
 	} else if me == nil {
 		err = errors.New("not found me")
-		return
+		return nil, err
 	}
-	return
+	return me, nil
 }
 
-func (r *queryResolver) Users(ctx context.Context, idOrName string) (users []*model.User, err error) {
+// Users is the resolver for the users field.
+func (r *queryResolver) Users(ctx context.Context, idOrName string) ([]*model.User, error) {
 	user, err := auth.GetUser(ctx)
 	if err != nil {
-		return
+		return nil, err
 	}
 	fmt.Printf("user: %v query idOrName: %s\n", user.Id, idOrName)
 
@@ -85,14 +79,14 @@ func (r *queryResolver) Users(ctx context.Context, idOrName string) (users []*mo
 	}
 	response, err := r.CassandraClient.Query(cassandra.QueryUserByIdGql, variables)
 	if err != nil {
-		return
+		return nil, err
 	}
 	fmt.Printf("query user by id response: %v\n", response)
 
 	mapUsers := make(map[string]*model.User)
 	queriedByIdUser, err := cassandra.GetUserFromQueryUserByIdResponse(response)
 	if err != nil {
-		return
+		return nil, err
 	} else if queriedByIdUser != nil {
 		mapUsers[queriedByIdUser.ID] = queriedByIdUser
 	}
@@ -103,14 +97,14 @@ func (r *queryResolver) Users(ctx context.Context, idOrName string) (users []*mo
 	for {
 		response, err = r.CassandraClient.Query(cassandra.QueryUserByNameGql, variables)
 		if err != nil {
-			return
+			return nil, err
 		}
 		fmt.Printf("query user by name response: %v\n", response)
 
 		queriedUsers, pageState, getErr := cassandra.GetUserFromQueryUserByNameResponse(response)
 		if getErr != nil {
 			err = getErr
-			return
+			return nil, err
 		}
 		for id, user := range queriedUsers {
 			mapUsers[id] = user
@@ -122,6 +116,15 @@ func (r *queryResolver) Users(ctx context.Context, idOrName string) (users []*mo
 		}
 	}
 
-	users = maps.Values(mapUsers)
-	return
+	users := maps.Values(mapUsers)
+	return users, nil
 }
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
