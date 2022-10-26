bb7341fb2ad72ee6e1126a09470548d04bf0763e
bryan.wu <bryan.wu@ringcentral.com>
2022-10-26 15:55:48 +0800

    support user service

--- service/user/deploy.sh
+++ service/user/deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-version=1.0.5
-./build.sh $version
+version=v0.1.0
+#./build.sh $version
+serviceName="user"
     
 pathToEnv0="../../0-env"
 kubeConfigFilePath=$(cat $pathToEnv0/which-env-to-apply)
 kubeConfigFilePath=$pathToEnv0"/"$kubeConfigFilePath
 echo "current env: $kubeConfigFilePath"
-sed -i.bak "s/auth:.*/auth:${version}/" ./k8s/deployment.yml
+sed -i.bak "s/$serviceName:.*/$serviceName:${version}/" ./k8s/deployment.yml
 kubectl --kubeconfig $kubeConfigFilePath apply -k ./k8s
 
