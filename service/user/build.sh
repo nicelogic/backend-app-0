43a827e1cd5fb18764181379191e0104aba1ef36
bryan.wu <bryan.wu@ringcentral.com>
2022-10-26 12:26:20 +0800

    support goproxy

--- service/user/build.sh
+++ service/user/build.sh
@@ -6,6 +6,6 @@ serviceName="user"
 echo "inpurt aliyun docker reposigory password:"
 docker login --username=niceice220 --password-stdin registry.cn-shanghai.aliyuncs.com
 docker build -t logic-base/$serviceName:$version ./
-docker tag logic-base/auth:$version registry.cn-shanghai.aliyuncs.com/logic-base/$serviceName:$version
+docker tag logic-base/$serviceName:$version registry.cn-shanghai.aliyuncs.com/logic-base/$serviceName:$version
 docker push registry.cn-shanghai.aliyuncs.com/logic-base/$serviceName:$version
     
