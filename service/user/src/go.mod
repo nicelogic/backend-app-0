6b33487171529c0618de5de878e75b54b8faf86f
bryan.wu <bryan.wu@ringcentral.com>
2022-11-26 11:45:06 +0800

    begin to support user crdb

--- service/user/src/go.mod
+++ service/user/src/go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	github.com/99designs/gqlgen v0.17.20
-	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-jwt/jwt v3.2.2+incompatible //indirect
 	github.com/vektah/gqlparser/v2 v2.5.1
 )
 
@@ -19,7 +19,6 @@ require (
 	github.com/jackc/pgtype v1.12.0 // indirect
 	github.com/jackc/pgx/v4 v4.17.2 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
-	github.com/machinebox/graphql v0.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/text v0.3.7 // indirect
@@ -33,11 +32,9 @@ require (
 	github.com/mitchellh/mapstructure v1.3.1
 	github.com/nicelogic/auth v0.1.0
 	github.com/nicelogic/authutil v0.3.0
-	github.com/nicelogic/cassandra v0.1.0
 	github.com/nicelogic/config v0.1.0
 	github.com/nicelogic/crdb v0.2.0
 	github.com/nicelogic/errs v0.1.0
 	github.com/rs/cors v1.8.2
-	golang.org/x/exp v0.0.0-20221025133541-111beb427cde
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- service/user/src/graph/schema.resolvers.go
+++ service/user/src/graph/schema.resolvers.go
@@ -5,14 +5,12 @@ package graph
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"user/cassandra"
 	"user/graph/generated"
 	"user/graph/model"
 
 	"github.com/nicelogic/auth"
-	"golang.org/x/exp/maps"
+	"github.com/nicelogic/authutil"
 )
 
 // UpdateUser is the resolver for the updateUser field.
@@ -22,48 +20,49 @@ func (r *mutationResolver) UpdateUser(ctx context.Context, changes map[string]in
 		return nil, err
 	}
 	fmt.Printf("user: %v update: %v\n", user.Id, changes)
+	return nil, nil
 
-	gql, variables, err := cassandra.UpdateUserGql(changes)
-	if err != nil {
-		return nil, err
-	}
-	variables["id"] = user.Id
-	response, err := r.CassandraClient.Mutation(gql, variables)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Printf("response: %v\n", response)
-	responseUser, err := cassandra.GetUpdatedUserFromResponse(response)
-	if err != nil {
-		return nil, err
-	}
-	return responseUser, nil
+	// gql, variables, err := cassandra.UpdateUserGql(changes)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// variables["id"] = user.Id
+	// response, err := r.CassandraClient.Mutation(gql, variables)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// fmt.Printf("response: %v\n", response)
+	// responseUser, err := cassandra.GetUpdatedUserFromResponse(response)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return responseUser, nil
 }
 
 // Me is the resolver for the me field.
 func (r *queryResolver) Me(ctx context.Context) (*model.User, error) {
-	user, err := auth.GetUser(ctx)
+	user, err := authutil.GetUser(ctx)
 	if err != nil {
 		return nil, err
 	}
 	fmt.Printf("user: %v query own info\n", user.Id)
-
-	variables := map[string]interface{}{
-		"id": user.Id,
-	}
-	response, err := r.CassandraClient.Query(cassandra.QueryUserByIdGql, variables)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Printf("response: %v\n", response)
-	me, err := cassandra.GetUserFromQueryUserByIdResponse(response)
-	if err != nil {
-		return nil, err
-	} else if me == nil {
-		err = errors.New("not found me")
-		return nil, err
-	}
-	return me, nil
+	return nil, nil
+	// variables := map[string]interface{}{
+	// 	"id": user.Id,
+	// }
+	// response, err := r.CassandraClient.Query(cassandra.QueryUserByIdGql, variables)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// fmt.Printf("response: %v\n", response)
+	// me, err := cassandra.GetUserFromQueryUserByIdResponse(response)
+	// if err != nil {
+	// 	return nil, err
+	// } else if me == nil {
+	// 	err = errors.New("not found me")
+	// 	return nil, err
+	// }
+	// return me, nil
 }
 
 // Users is the resolver for the users field.
@@ -73,51 +72,51 @@ func (r *queryResolver) Users(ctx context.Context, idOrName string) ([]*model.Us
 		return nil, err
 	}
 	fmt.Printf("user: %v query idOrName: %s\n", user.Id, idOrName)
+	return nil, nil
+	// variables := map[string]interface{}{
+	// 	"id": idOrName,
+	// }
+	// response, err := r.CassandraClient.Query(cassandra.QueryUserByIdGql, variables)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// fmt.Printf("query user by id response: %v\n", response)
 
-	variables := map[string]interface{}{
-		"id": idOrName,
-	}
-	response, err := r.CassandraClient.Query(cassandra.QueryUserByIdGql, variables)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Printf("query user by id response: %v\n", response)
+	// mapUsers := make(map[string]*model.User)
+	// queriedByIdUser, err := cassandra.GetUserFromQueryUserByIdResponse(response)
+	// if err != nil {
+	// 	return nil, err
+	// } else if queriedByIdUser != nil {
+	// 	mapUsers[queriedByIdUser.ID] = queriedByIdUser
+	// }
 
-	mapUsers := make(map[string]*model.User)
-	queriedByIdUser, err := cassandra.GetUserFromQueryUserByIdResponse(response)
-	if err != nil {
-		return nil, err
-	} else if queriedByIdUser != nil {
-		mapUsers[queriedByIdUser.ID] = queriedByIdUser
-	}
+	// variables = map[string]interface{}{
+	// 	"name": idOrName,
+	// }
+	// for {
+	// 	response, err = r.CassandraClient.Query(cassandra.QueryUserByNameGql, variables)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	fmt.Printf("query user by name response: %v\n", response)
 
-	variables = map[string]interface{}{
-		"name": idOrName,
-	}
-	for {
-		response, err = r.CassandraClient.Query(cassandra.QueryUserByNameGql, variables)
-		if err != nil {
-			return nil, err
-		}
-		fmt.Printf("query user by name response: %v\n", response)
+	// 	queriedUsers, pageState, getErr := cassandra.GetUserFromQueryUserByNameResponse(response)
+	// 	if getErr != nil {
+	// 		err = getErr
+	// 		return nil, err
+	// 	}
+	// 	for id, user := range queriedUsers {
+	// 		mapUsers[id] = user
+	// 	}
+	// 	if pageState == "" {
+	// 		break
+	// 	} else {
+	// 		variables["pageState"] = pageState
+	// 	}
+	// }
 
-		queriedUsers, pageState, getErr := cassandra.GetUserFromQueryUserByNameResponse(response)
-		if getErr != nil {
-			err = getErr
-			return nil, err
-		}
-		for id, user := range queriedUsers {
-			mapUsers[id] = user
-		}
-		if pageState == "" {
-			break
-		} else {
-			variables["pageState"] = pageState
-		}
-	}
-
-	users := maps.Values(mapUsers)
-	return users, nil
+	// users := maps.Values(mapUsers)
+	// return users, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
