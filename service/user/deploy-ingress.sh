cd41cb08d26058df11b656fe26ce06e126761137
bryan.wu <bryan.wu@ringcentral.com>
2022-11-27 15:52:16 +0800

    refactor user service(release v0.2.0)

---
+++ service/user/deploy-ingress.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pathToEnv0="../../0-env"
+kubeConfigFilePath=$(cat $pathToEnv0/which-env-to-apply)
+kubeConfigFilePath=$pathToEnv0"/"$kubeConfigFilePath
+echo "current env: $kubeConfigFilePath"
+kubectl --kubeconfig $kubeConfigFilePath apply -f ./k8s/ingress-route.yml
+
